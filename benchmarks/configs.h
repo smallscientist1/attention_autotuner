ca9a5cba54c7e313e1ab1b17828bf1ab795333cc
FeiyangChen <v-feiychen@microsoft.com>
2024-03-27 07:13:23 +0000

    remove some template arg

--- benchmarks/configs.h
+++ benchmarks/configs.h
@@ -1,8 +1,7 @@
 template<int Br_, int Bc_,int Kd_, int D_, int Nthreads_, 
 /*smem_fuse only*/ int warps_mma1_N_ = 1, int warps_mma_N_ = 1, 
-int BlockKSmem_=Kd_, int num_stages_qk_=1, int BlockKSmem2_=Bc_, int num_stages_v_=1, int SmemKAtom_=64, bool unrollLastIter_=true,
-/*retnet*/int SmemKAtomMask_ = 64, int num_stages_mask_ = 1, 
-/*smem_fuse only*/int SmemKAtomV_ = 64>
+int BlockKSmem_=Kd_, int num_stages_qk_=1, int BlockKSmem2_=Bc_, int num_stages_v_=1, bool unrollLastIter_=true,
+/*retnet*/int num_stages_mask_ = 1>
 class ImplementShape{
 public:
     constexpr static int Br = Br_;
@@ -15,12 +14,12 @@ public:
     constexpr static bool load_q_once = (BlockKSmem == Kd);
     constexpr static int BlockKSmem2 = BlockKSmem2_;
     constexpr static int num_stages_v = num_stages_v_;
-    constexpr static int SmemKAtom = SmemKAtom_;
+    constexpr static int SmemKAtom = BlockKSmem % 64 == 0 ? 64 : 32;
     constexpr static int kSwizzle = SmemKAtom == 32 ? 2 : 3;
     constexpr static bool unrollLastIter = unrollLastIter_;
 
     /*smem_fuse*/
-    constexpr static int SmemKAtomV = SmemKAtomV_;
+    constexpr static int SmemKAtomV = D % 64 == 0 ? 64 : 32;
     constexpr static int kSwizzleV = SmemKAtomV == 32 ? 2 : 3;
     constexpr static int SmemKAtomP = Bc % 64 == 0 ? 64 : 32;
     constexpr static int kSwizzleP = SmemKAtomP == 32 ? 2 : 3;
@@ -30,6 +29,7 @@ public:
     constexpr static int warps_mma_N = warps_mma_N_;
 
     /*retnet*/
-    constexpr static int SmemKAtomMask = SmemKAtomMask_;
+    constexpr static int SmemKAtomMask = Bc % 64 == 0 ? 64 : 32;
     constexpr static int kSwizzleMask = SmemKAtomMask == 32 ? 2 : 3;
+    constexpr static int num_stages_mask = num_stages_mask_;
 };
